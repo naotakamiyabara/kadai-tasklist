3c672de9122166e8df5068b9e30032e27dab72bb
naotaka.miyabara <na.miya@kdk-nagano.co.jp>
2021-06-19 01:27:02 +0000

    validation

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -43,6 +43,11 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        // バリデーション
+        $request->validate([
+            'content' => 'required|max:255',
+        ]);
+        
         // メッセージを作成
         $task = new Task;
         $task->content = $request->content;
@@ -97,6 +102,11 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        // バリデーション
+        $request->validate([
+            'content' => 'required|max:255',
+        ]);
+        
         // idの値でメッセージを検索して取得
         $task = Task::findOrFail($id);
         // メッセージを更新
